8c7a368ea0c77eb4292ce1d5ce2df1fb7916da0c
Naiara <naiara.benito@gmail.com>
2023-03-05 21:35:21 +0100

    Código de Orden.java + ConfirmarBorradoOrdenDialog.java depurado y comentado

--- app/src/main/java/com/example/proyecto_individual_naiarabenito/ui/cesta/ConfirmarBorradoOrdenDialog.java
+++ app/src/main/java/com/example/proyecto_individual_naiarabenito/ui/cesta/ConfirmarBorradoOrdenDialog.java
@@ -1,5 +1,8 @@
+
+// _____________________________________ UBICACIÓN DEL PAQUETE _____________________________________
 package com.example.proyecto_individual_naiarabenito.ui.cesta;
 
+// ______________________________________ PAQUETES IMPORTADOS ______________________________________
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -7,62 +10,93 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentManager;
 
-import com.example.proyecto_individual_naiarabenito.R;
 
+/* ############################### CLASE CONFIRMAR_BORRADO_ORDEN_DIALOG ############################
+    *) Descripción:
+        La función de esta clase es mostrar y gestionar la configuración del dialogo de confirmación
+        de compra.
+
+    *) Tipo: DialogFragment
+*/
 public class ConfirmarBorradoOrdenDialog extends DialogFragment {
 
-    private ConfirmarBorradoOrdenDialog.ListenerDialogo listener;
+// ___________________________________________ Interfaz ____________________________________________
+    private ConfirmarBorradoOrdenDialog.ListenerDialogo listener;   // Listener del Diálogo
+
+    // Interfaz para gestionar la acción confirmar la eliminación del producto
     public interface ListenerDialogo{
         void setPositiveButton(String produto, int position);
     }
 
+    // Asignar valor al listener del Diálogo
     @Override
     public void onAttach(@NonNull Activity context) {
         super.onAttach(context);
         try{
+            // Inicializar el listener
             listener = (ListenerDialogo) this.context;
-        }catch (ClassCastException e){
+        }catch (ClassCastException e){  // En caso de excepción
+            // Notificar error
             throw new ClassCastException(this.toString() + " debe implementar ListenerDialogo");
         }
     }
 
-    private String producto;
-    private int position;
-    private Context context;
-    public ConfirmarBorradoOrdenDialog(String producto, int position, Context context){
-        this.producto = producto;
-        this.position = position;
-        this.context = context;
+// ___________________________________________ Variables ___________________________________________
+    private final String producto;    // Nombre del producto que se desea eliminar
+    private final int position;       // Posición en la cesta del producto que se desea eliminar
+    private final Context context;    // Contexto del Activity Cesta Fragment
+
+// __________________________________________ Constructor __________________________________________
+    public ConfirmarBorradoOrdenDialog(String pProducto, int pPosition, Context pContext){
+        this.producto = pProducto;
+        this.position = pPosition;
+        this.context = pContext;
     }
 
-    public Dialog onCreateDialog (@Nullable Bundle savedInstanceState, String producto, int position) {
-        super.onCreateDialog(savedInstanceState);
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+// ____________________________________________ Métodos ____________________________________________
 
-        builder.setTitle("¡Espera!");
-        builder.setMessage("¿Seguro que deseas eliminar el producto de la cesta?")
-                .setPositiveButton("Seguro", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+/*  Método onCreateDialog:
+    ----------------------
+        *) Parámetros (Input):
+                1) (Bundle) savedInstanceState: Contiene el diseño predeterminado del Diálogo.
+        *) Parámetro (Output):
+                (Dialog) dialogo: Contiene el objeto Diálogo de confirmación.
+        *) Descripción:
+                Este método se ejecuta cuando el usuario se intenta eliminar un producto de la cesta.
+*/
+    public Dialog onCreateDialog (@Nullable Bundle savedInstanceState) {
+        super.onCreateDialog(savedInstanceState);
+
+        // Crear el Diálogo
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("¡Espera!");   // Asignar título
+        builder.setMessage("¿Seguro que deseas eliminar el producto de la cesta?")  // Asignar texto
+                .setPositiveButton("Seguro", new DialogInterface.OnClickListener() {    // Asignar botón de confirmación
+                    public void onClick(DialogInterface dialog, int id) {   // Si se ha pulsado
+                        // Informar de la eliminación del producto
                         Toast.makeText(getContext(), "Se ha eliminado", Toast.LENGTH_LONG).show();
+
+                        // Eliminar producto de la cesta
                         listener.setPositiveButton(producto, position);
+
+                        // Eliminar Diálogo
                         dismiss();
                     }
                 })
-                .setNegativeButton("Me he arrepentido", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
+                .setNegativeButton("Me he arrepentido", new DialogInterface.OnClickListener() {   // Asignar botón de rechazo
+                    public void onClick(DialogInterface dialog, int id) {   // Si se ha pulsado
+                        // Informar de la decisión
                         Toast.makeText(getContext(), "No se ha eliminado", Toast.LENGTH_LONG).show();
+
+                        // Eliminar Diálogo
                         dismiss();
                     }
                 });
-        // Create the AlertDialog object and return it
+        // Crear el objeto AlertDialog
         return builder.create();
     }
-
-
 }
--- app/src/main/java/com/example/proyecto_individual_naiarabenito/ui/cesta/Orden.java
+++ app/src/main/java/com/example/proyecto_individual_naiarabenito/ui/cesta/Orden.java
@@ -1,14 +1,25 @@
+
+// _____________________________________ UBICACIÓN DEL PAQUETE _____________________________________
 package com.example.proyecto_individual_naiarabenito.ui.cesta;
 
+
+/* ########################################### CLASE ORDEN #########################################
+    *) Descripción:
+        La función de esta clase es mostrar y gestionar la configuración del carrito.
+
+    *) Tipo: Class
+*/
 public class Orden {
 
-    public int id;
-    public String nombreProd;
-    public double precioProd;
-    public int imagenProd;
-    public int cantidadProd;
-    public String emailUsuario;
+// ___________________________________________ Variables ___________________________________________
+    private int id;                 // Id de la orden
+    private String nombreProd;      // Nombre del producto de la orden
+    private double precioProd;      // Precio por unidad del producto de la orden
+    private int imagenProd;         // Foto del producto de la orden
+    private int cantidadProd;       // Cantidad del producto de la orden
+    private String emailUsuario;    // Email del usuario de la orden
 
+// __________________________________________ Constructor __________________________________________
     public Orden(String nombreProd, double precioProd, int imagenProd, int cantidadProd, String emailUsuario) {
         this.nombreProd = nombreProd;
         this.precioProd = precioProd;
@@ -17,51 +28,85 @@ public class Orden {
         this.emailUsuario = emailUsuario;
     }
 
+// _________________________________________________________________________________________________
+
+    public Orden() {}
+
+// _______________________________________ Getters y Setters _______________________________________
+
+/*  Método getId:
+    -------------
+        *) Parámetros (Input):
+        *) Parámetro (Output):
+                (int) id: Id de la orden del pedido.
+        *) Descripción:
+                Getter del id de la orden del pedido.
+*/
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
+// _________________________________________________________________________________________________
+
+    public void setId(int pId) {
+        this.id = pId;
     }
 
-    public Orden() {}
+// _________________________________________________________________________________________________
 
     public String getNombreProd() {
         return nombreProd;
     }
 
+// _________________________________________________________________________________________________
+
     public void setNombreProd(String nombreProd) {
         this.nombreProd = nombreProd;
     }
+
+// _________________________________________________________________________________________________
 
     public double getPrecioProd() {
         return precioProd;
     }
 
+// _________________________________________________________________________________________________
+
     public void setPrecioProd(double precioProd) {
         this.precioProd = precioProd;
     }
+
+// _________________________________________________________________________________________________
 
     public int getImagenProd() {
         return imagenProd;
     }
 
+// _________________________________________________________________________________________________
+
     public void setImagenProd(int imagenProd) {
         this.imagenProd = imagenProd;
     }
+
+// _________________________________________________________________________________________________
 
     public int getCantidadProd() {
         return cantidadProd;
     }
 
+// _________________________________________________________________________________________________
+
     public void setCantidadProd(int cantidadProd) {
         this.cantidadProd = cantidadProd;
     }
 
+// _________________________________________________________________________________________________
+
     public String getEmailUsuario() {
         return emailUsuario;
     }
+
+// _________________________________________________________________________________________________
 
     public void setEmailUsuario(String emailUsuario) {
         this.emailUsuario = emailUsuario;
