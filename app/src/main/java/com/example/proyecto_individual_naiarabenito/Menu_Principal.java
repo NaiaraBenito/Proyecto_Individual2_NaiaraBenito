f29598ad346c4e9e7b12f24089abb348d9b29228
Naiara <naiara.benito@gmail.com>
2023-03-05 17:44:18 +0100

    Código de Menu_Principal.java depurado y comentado

--- app/src/main/java/com/example/proyecto_individual_naiarabenito/Menu_Principal.java
+++ app/src/main/java/com/example/proyecto_individual_naiarabenito/Menu_Principal.java
@@ -1,8 +1,9 @@
+
+// _____________________________________ UBICACIÓN DEL PAQUETE _____________________________________
 package com.example.proyecto_individual_naiarabenito;
 
+// ______________________________________ PAQUETES IMPORTADOS ______________________________________
 import android.Manifest;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
@@ -10,15 +11,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
-
-import com.example.proyecto_individual_naiarabenito.ui.inicio.InicioFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -26,25 +19,45 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.example.proyecto_individual_naiarabenito.databinding.ActivityMenuPrincipalBinding;
 
+
+/* ###################################### CLASE MENU_PRINCIPAL #####################################
+    *) Descripción:
+        La función de esta clase es mostrar y gestionar la vista de toda la aplicación.
+
+    *) Tipo: Activity
+*/
 public class Menu_Principal extends AppCompatActivity {
 
-    private static final int REQUEST_CALL = 1;
-    private ActivityMenuPrincipalBinding binding;
-    private String email;
+// ___________________________________________ Variables ___________________________________________
+    private static final int REQUEST_CALL = 1;      // ID para realizar llamadas telefónicas
+    private ActivityMenuPrincipalBinding binding;   // Variable para gestionar los fragmentos del menú
 
+// ____________________________________________ Métodos ____________________________________________
+
+/*  Método onCreate:
+    ----------------
+        *) Parámetos (Input):
+                1) (Bundle) savedInstanceState: Contiene el diseño predeterminado del Activity.
+        *) Parámetro (Output):
+                void
+        *) Descripción:
+                Éste método se ejecuta cuando el usuario se ha logueado correctamente. Gestiona los
+                fragmentos del menú
+*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Cargar las preferencias configuradas por el usuario
         cargar_configuracion();
+
+        // Crear la vista
         super.onCreate(savedInstanceState);
+
+        // Gestionar los fragmentos
         binding = ActivityMenuPrincipalBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_inicio, R.id.navigation_cesta, R.id.navigation_perfil, R.id.navigation_configuracion)
                 .build();
@@ -54,34 +67,68 @@ public class Menu_Principal extends AppCompatActivity {
 
     }
 
+// _________________________________________________________________________________________________
+
+/*  Método llamada:
+    -------------------
+        *) Parámetos (Input):
+            1) (View) v: Vista asociada al Activity actual
+        *) Parámetro (Output):
+                void
+        *) Descripción:
+                Éste método se ejecuta cuando el usuario pulsa el número de teléfono del inicio.
+                Comprueba el permiso de llamada:
+                    - Si tiene el permiso: Realiza la llamada al número.
+                    - Si no tiene el permiso: Pide permiso para realizar llamadas.
+*/
     public void llamada(View v){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+        // Comprueba que la aplicación tenga permiso para realizar llamadas telefónicas
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){   // Si no tiene permiso
+            // Pide permiso para poder realizar llamadas telefónicas
             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
-        } else{
+        } else{     // Si tiene permiso: Realiza la llamada al número
             startActivity(new Intent(Intent.ACTION_CALL, Uri.parse("tel:943466484")));
         }
     }
 
+// _________________________________________________________________________________________________
+
+/*  Método cargar_configuracion:
+    ----------------------------
+        *) Parámetos (Input):
+        *) Parámetro (Output):
+                void
+        *) Descripción:
+                Éste método carga las preferencias configuradas por el usuario (modo oscuro,
+                orientación de la pantalla...).
+*/
     private void cargar_configuracion(){
 
+        // Obtener las preferencias configuradas por el usuario
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
 
+        // Comprobar el estado de la preferencia del modo oscuro
         boolean modoOscuro = sp.getBoolean("modo_oscuro", false);
 
-        if(modoOscuro){
-
+        if(modoOscuro){     // Si el modo oscuro está activado: Pintar el fondo de gris
             getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.gris_claro));
-        } else{
+        } else{             // Si el modo oscuro está desactivado: Pintar el fondo de blanco
             getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.white));
         }
 
-        String ori = sp.getString("orientacion","1");
-        if("1".equals(ori)){
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-        } else if("2".equals(ori)){
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        } else if("3".equals(ori)){
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        // Comprobar el estado de la preferencia de la orientación
+        String ori = sp.getString("orientacion","false");
+        switch (ori) {
+            case "1":       // Si la orientación es 1: Desbloquear el giro automático de la app
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                break;
+            case "2":       // Si la orientación es 2: Bloquear la orientacion vertical
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+            case "3":       // Si la orientación es 3: Bloquear la orientacion horizontal
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
         }
     }
 }
